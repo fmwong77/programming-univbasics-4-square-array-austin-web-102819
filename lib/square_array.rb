a42e15d366ea5b4498218e850d70fa36f2a02a51
Mei Wong <fongmeiw@gmail.com>
2019-09-20 17:01:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
+  arr = []
   counter = 0
   while array(counter) do
-    array[counter] = Math.sqrt(array[counter])
+    arr[counter] = array[counter]
     counter = counter + 1
 end
 
