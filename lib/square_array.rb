702018d1d0425cd1e38f4ee13af1a94e66c68b64
Mei Wong <fongmeiw@gmail.com>
2019-09-20 17:12:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   arr = []
   counter = 0
   while array(counter) do
-    arr[counter] << array[counter]*array[counter]
+    arr << array[counter]*array[counter]
     counter = counter + 1
 end
 p arr
